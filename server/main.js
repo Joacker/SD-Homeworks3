bd2d8162eb2c05298467581d9d74e8d2738c5638
Joako-Sador <jfernandezvizcarra@gmail.com>
2022-06-10 18:47:47 -0400

    new commit

--- server/main.js
+++ server/main.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const { cassandra } = require('cassandra-driver');
+const cassandra = require('cassandra-driver');
 
 //-------------------------------------------
 
@@ -22,12 +22,33 @@ app.use(cors());
 
 var port = process.env.PORT || 3000;
 var host = process.env.PORT || '0.0.0.0';
+//let authProvider = new cassandra.auth.PlainTextAuthProvider('Username', 'Password');
 ///////////////////////////////////////////////////////////////
 
-
+const client = new cassandra.Client({
+  contactPoints: ['cassandra-cluster:9042', 'cassandra-cluster:9142', 'cassandra-cluster:9242'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'test',
+  authProvider: new PlainTextAuthProvider('cassandra', 'cassandra')
+});
 
 app.get("/", (req, res) => {
   res.send("ola api");
+});
+let query = `SELECT * FROM months`
+app.post("/create", async (req, res) => {
+  const { nombre, apellido, rut, email, fecha_nacimiento, comentario, farmacos, doctor } = req.body
+  let parameters = [rut];
+    await client.execute(query,parameters, (error, result) => {
+      if (error != undefined) {
+        console.log(error);
+      }else {
+        console.log(result);
+        res.json(result.rows);
+      }
+
+    })
+
 });
 
 
