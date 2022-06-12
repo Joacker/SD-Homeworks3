eec38d77d36b908c621efb685ce227186194ac1b
Joako-Sador <jfernandezvizcarra@gmail.com>
2022-06-12 16:51:33 -0400

    create ready

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cassandra = require('cassandra-driver');
-const dse = require('dse-driver');
+const { v4: uuidv4 } = require('uuid');
 //-------------------------------------------
 
 /* CONFIGS */
@@ -22,33 +22,68 @@ app.use(cors());
 
 var port = process.env.PORT || 3000;
 var host = process.env.PORT || '0.0.0.0';
-//let authProvider = new cassandra.auth.PlainTextAuthProvider('cassandra', 'Password');
+
 ///////////////////////////////////////////////////////////////
+
 var PlainTextAuthProvider = cassandra.auth.PlainTextAuthProvider;
 
 const client = new cassandra.Client({
   contactPoints: ['cassandra-node1','cassandra-node2','cassandra-node3'],
   localDataCenter: 'datacenter1',
-  keyspace: 'sampledata',
+  keyspace: 'medic_1',
+  authProvider: new PlainTextAuthProvider('cassandra', 'cassandra')
+});
+
+const client2 = new cassandra.Client({
+  contactPoints: ['cassandra-node1','cassandra-node2','cassandra-node3'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'medic_2',
   authProvider: new PlainTextAuthProvider('cassandra', 'cassandra')
 });
 
 app.get("/", (req, res) => {
   res.send("ola api");
 });
-let query = `SELECT * FROM months`
 
-app.get("/create", async (req, res) => {
-    await client.execute(query, (error, result) => {
-      if (error != undefined) {
-        console.log(error);
-      }else {
-        console.log(result);
-        res.json(result.rows);
+app.post('edit',(req,res)=>{
+  
+});
+
+app.post('/create', (req, res) => {
+  (async () => {
+    const query = 'SELECT * FROM pacientes WHERE rut=? ALLOW FILTERING';
+    const query2 = "INSERT INTO pacientes (id, nombre,apellido,rut,email,fecha_nacimiento) VALUES(?,?,?,?,?,?);";
+    const query3 = "INSERT INTO recetas (id, id_paciente,comentario,farmacos,doctor) VALUES(?,?,?,?,?);";
+    const receta = req.body;
+    var gen_id_1 = uuidv4();
+    var gen_id_2 = uuidv4();
+    console.log("Receta: ",receta)
+    console.log("ID gen para Paciente: ",gen_id_1)
+    console.log("ID gen para Receta: ",gen_id_2)
+
+    await client.execute(query,[receta.rut]).then(result => {
+      console.log('Result ' + result.rows[0]);
+
+      if(result.rows[0] != undefined){
+        console.log("existe el paciente")
+        client2.execute(query3,[gen_id_2,result.rows[0].id,receta.comentario,receta.farmacos,receta.doctor]).then(result2 => {
+          console.log('Result ' + result2);
+          res.json({'ID_Receta': gen_id_2})
+        }).catch((err) => {console.log('ERROR:', err);});
+
+      }else{
+        console.log("No existe el paciente")
+        client.execute(query2,[gen_id_1,receta.nombre,receta.apellido,receta.rut,receta.email,receta.fecha_nacimiento]).then(result => {
+          console.log('Result ' + result);
+          client2.execute(query3,[gen_id_2,gen_id_1,receta.comentario,receta.farmacos,receta.doctor]).then(result2 => {
+            console.log('Result ' + result2);
+            res.json({'ID_Receta': gen_id_2})
+          }).catch((err) => {console.log('ERROR:', err);});
+        }).catch((err) => {console.log('ERROR:', err);});
       }
-
-    })
-
+    }).catch((err) => {console.log('ERROR:', err);});
+  })();
+  
 });
 
 
